3d28807cb911d7bfe88b7963c914e33cab4abdc8
kenan <kenabdulka@gmail.com>
2020-04-30 15:19:30 -0700

    set up event listeners, successfully read input from user and created an intitialization function

--- budget.js
+++ budget.js
@@ -1,41 +1,84 @@
+
+
 var budgetController = (function() {
 
-	var x = 25;
-
-	var add = function(a) {
-		return x + a;
-	}
-
-	return {
-		publicTest: function(b) {
-			console.log(add(b));
-			return add(b)
-		}
-	}
 
 })();
 
 // create UI controller module
 var UIController = (function() {
 
+	var DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: ".add__description",
+		inputValue: ".add__value",
+		inputBtn: ".add__btn"
+	};
+
+    // make the retrieved input values public
+    return {
+    	getInput: function() {
+	    	return {
+	    		// either inc or exp
+	    		type: document.querySelector(DOMstrings.inputType).value,
+	    		description: $(DOMstrings.inputDescription).val(),
+	    		value: $(DOMstrings.inputValue).val()
+	    	};
+    	},
+
+    	// expose DOMstrings to be public
+    	getDOMstrings: function() {
+    		return DOMstrings;
+    	}
+    };
+
 })();
 
 // create this module to enable UIController and budgetController to 
 // interact and know about each other
-var controller = (function(budgetCtrl, UICtrl){
+// Global app controller
+var controller = (function(budgetCtrl, UICtrl) {
 
-	var test = budgetCtrl.publicTest(5);
+	var setupEventListeners = function() {
 
-	// returns an object so we can use the data inside
+		var DOM = UICtrl.getDOMstrings();
+
+		$(document).keypress(function (event) {
+    		if (event.key === "Enter" || event.which === 13) {
+        	// alert('enter key is pressed');
+        		ctrllAddItem();
+    		}
+		});
+
+		$(DOM.inputBtn).click(ctrllAddItem);
+
+	};
+
+	var ctrllAddItem = function() {
+		// get field input data
+		var input = UICtrl.getInput();
+		console.log("testing for " + input);
+		// add the item to the budget controller
+
+		// add the new item to the user interface 
+
+		// calculate the budget
+
+		// displa the budget on the UI
+		alert(input.value + " " + input.description + " " + input.type);
+	};
+
+
 	return {
-		publicTest2: function() {
-			console.log(test);
-			return test;
+		init: function() {
+			console.log("Application has started");
+			setupEventListeners();
 		}
-	}
+	};
 
 })(budgetController, UIController);
 
+controller.init();
 
 
 
@@ -46,6 +89,31 @@ var controller = (function(budgetCtrl, UICtrl){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// var controller = (function(budgetCtrl, UICtrl){
+
+// 	var test = budgetCtrl.publicTest(5);
+
+// 	// returns an object so we can use the data inside
+// 	return {
+// 		publicTest2: function() {
+// 			console.log(test);
+// 			return test;
+// 		}
+// 	}
+
+// })(budgetController, UIController);
 
 
 
